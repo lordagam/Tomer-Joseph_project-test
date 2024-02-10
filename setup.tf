c73c7aeaad142ab59442481bb6bf4956d59851f7
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-02-10 20:09:39 +0200

    Add files via upload

--- setup.tf
+++ setup.tf
@@ -1,6 +1,6 @@
 resource "terraform_data" "clusterConfigutration" {
   provisioner "local-exec" {
-    command = "aws eks --region eu-west-3 update-kubeconfig --name eks-cluster"
+    command = "aws eks --region ap-northeast-3 update-kubeconfig --name eks-cluster"
   }
   depends_on = [aws_eks_cluster.eks-cluster  ]
 }
@@ -12,6 +12,31 @@ resource "terraform_data" "installIngress" {
   depends_on = [aws_eks_addon.addons]
 }
 
+resource "terraform_data" "ns-cert-manager" {
+  provisioner  "local-exec" {
+    command = "kubectl create namespace cert-manager"
+  }
+  depends_on = [terraform_data.installIngress]
+}
+
+
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  namespace = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  set {
+    name  = "issuer_email"
+    value = "tomeroz802@gmail.com"
+  }
+  depends_on = [terraform_data.ns-cert-manager]
+}
 
 resource "helm_release" "argocd" {
   name             = "argocd"
@@ -23,17 +48,6 @@ resource "helm_release" "argocd" {
   values           = [file("values/argocd.yaml")]
 
   depends_on = [ terraform_data.installIngress]
-}
-# helm install argocd -n argocd -f values/argocd.yaml
-
-
-resource "terraform_data" "ApplyIngress" {
-  provisioner "local-exec" {
-    command = "kubectl apply -f argo-ingress.yaml"
-  }
-
-  depends_on = [ helm_release.argocd ]
-  
 }
 
 resource "aws_route53_zone" "primary" {
